97bbe2e68d64e917bc789caee650ecbff6df9ce3
Matross-20 <matrossmthawelanga@gmail.com>
2024-09-24 16:20:01 +0200

    FiXit

--- challenge/4-delete_dnodeint/lists.h
+++ challenge/4-delete_dnodeint/lists.h
@@ -1,18 +1,26 @@
-#include "lists.h"
-#include <stdlib.h>
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
 
 /**
- * free_dlistint - Frees a doubly linked list
- * @head: The pointer to the head of the doubly linked list
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
  */
-void free_dlistint(dlistint_t *head)
+typedef struct dlistint_s
 {
-    dlistint_t *tmp;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 
-    while (head != NULL)
-    {
-        tmp = head;
-        head = head->next;
-        free(tmp);
-    }
-}
+void free_dlistint(dlistint_t *head);  /* Ensure this is declared */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+size_t print_dlistint(const dlistint_t *h);
+
+#endif /* LISTS_H */
